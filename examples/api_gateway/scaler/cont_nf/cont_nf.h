71218dd8eb12ea7ad230ee014c7b75f47c3b0a15
Kevin Deems <kevin8deems@gmail.com>
2021-03-11 01:21:03 +0000

    Got container running through docker stack

--- examples/api_gateway/scaler/cont_nf/cont_nf.h
+++ examples/api_gateway/scaler/cont_nf/cont_nf.h
@@ -3,8 +3,8 @@
 #include <sys/stat.h>
 #include <unistd.h>
 
-#define CONT_RX_PIPE_NAME "rx_pipe"
-#define CONT_TX_PIPE_NAME "tx_pipe"
+#define CONT_RX_PIPE_NAME "/rx_pipe"
+#define CONT_TX_PIPE_NAME "/tx_pipe"
 
 int
 create_pipes(void);
